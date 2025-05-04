6a04337612ffa210a4327b64c8b3ed143eed3abd
Timur Bakiiev <volkprizrak124@gmail.com>
2025-05-04 21:16:25 +0200

    reactHW-1

---
+++ src/task-3.ts
@@ -0,0 +1,13 @@
+const usernames: string[] = ["Alice", "Bob", "Charlie"];
+const ratings: number[] = [5, 4, 3, 5];
+
+interface Product {
+  id: number;
+  title: string;
+}
+
+const products: Product[] = [
+  { id: 1, title: "Tablet" },
+  { id: 2, title: "Laptop" },
+  { id: 3, title: "Smartphone" },
+]; 
